532828d33af4c2e288b6417e48ec09b56c96c94b
Engin Polat <polatengin@hotmail.com>
2023-01-13 19:05:15 +0000

    adding session session to the builder

--- Program.cs
+++ Program.cs
@@ -3,6 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSession(options =>
+{
+  options.IdleTimeout = TimeSpan.FromMinutes(30);
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
