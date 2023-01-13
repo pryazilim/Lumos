1e4f60a5a9c687ad92faf53025288778e8779475
Engin Polat <polatengin@hotmail.com>
2023-01-13 18:50:17 +0000

    adding admin area route

--- Program.cs
+++ Program.cs
@@ -8,11 +8,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  app.UseExceptionHandler("/Home/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 
-    app.UseHttpsRedirection();
+  app.UseHttpsRedirection();
 }
 
 app.UseStaticFiles();
@@ -20,6 +20,11 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.MapAreaControllerRoute(
+    name: "Admin",
+    areaName: "Admin",
+    pattern: "Admin/{controller=AdminDashboard}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
     name: "default",
