c2a44a863abe21aaa88dcbc20cb7ea5755ced2aa
Emirhan Vurhan <emirhan@pryazilim.com>
2023-01-16 16:08:41 +0300

    AuthenticationGuard fixed

--- Areas/Admin/Filters/AuthenticationGuardFilter.cs
+++ Areas/Admin/Filters/AuthenticationGuardFilter.cs
@@ -3,11 +3,46 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 public class AuthenticationGuardAttribute : ActionFilterAttribute
 {
+  public string? Page { get; set; }
   public override void OnActionExecuting(ActionExecutingContext filterContext)
   {
+    string action = Page!;
+
     if (string.IsNullOrWhiteSpace(filterContext.HttpContext.Session.GetString("CurrentUser")))
     {
-      filterContext.Result = new RedirectToRouteResult(routeName: "AdminLogin", permanent: false, routeValues: null);
+      filterContext.Result = new RedirectToActionResult("Login", "", null);
+    }
+    else
+    {
+      string? currentUser;
+
+      var currentUserValue = filterContext.HttpContext.Session.GetString("CurrentUser");
+
+      var currentUserId = Convert.ToInt32(currentUserValue);
+
+      using (var db = new DataContext())
+      {
+        currentUser = db.AdminUser!.Where(x => x.Id == currentUserId).Select(x => x.Permission).FirstOrDefault();
+      }
+
+      List<string>? permissionList = currentUser!.Split(new char[] { '\"' }).ToList();
+
+      if (currentUser == null)
+      {
+        filterContext.Result = new RedirectToActionResult("Login", "", null);
+      }
+
+      if(!permissionList.Any(x => x == action))
+      {
+        if (permissionList.Any(x => x == "Index"))
+        {
+          filterContext.Result = new RedirectResult("/admin");
+        }
+        else
+        {
+          filterContext.Result = new RedirectToActionResult("Login", "", null);
+        }
+      }
     }
   }
 }
