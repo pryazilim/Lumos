c7b6a16b90979098a9e4ad330eb59c7101cbe69b
Emirhan Vurhan <emirhan@pryazilim.com>
2023-01-16 12:12:30 +0300

    base model and admin user model added

---
+++ Database/DbModel/AdminUser.cs
@@ -0,0 +1,6 @@
+public class AdminUser : BaseModel
+{
+  public string? Permission { get; set; }
+  public string? UserName { get; set; }
+  public string? Password { get; set; }
+}
---
+++ Database/DbModel/BaseModel.cs
@@ -0,0 +1,6 @@
+public class BaseModel
+{
+  public int Id { get; set; }
+  public DateTime CreatedDate { get; set; }
+  public DateTime UpdatedDate { get; set; }
+}
