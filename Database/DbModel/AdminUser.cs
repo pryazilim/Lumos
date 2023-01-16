fadcbc00e2d428334cd7a907b103922c629eb3bd
Emirhan Vurhan <emirhan@pryazilim.com>
2023-01-16 16:07:53 +0300

    db model values changed

--- Database/DbModel/AdminUser.cs
+++ Database/DbModel/AdminUser.cs
@@ -1,5 +1,8 @@
-public class AdminUser : BaseModel
+public class AdminUser
 {
+  public int Id { get; set; }
+  public DateTime? CreatedDate { get; set; }
+  public DateTime? UpdatedDate { get; set; }
   public string? Permission { get; set; }
   public string? UserName { get; set; }
   public string? Password { get; set; }
--- Database/DbModel/BaseModel.cs
+++
@@ -1,6 +0,0 @@
-public class BaseModel
-{
-  public int Id { get; set; }
-  public DateTime CreatedDate { get; set; }
-  public DateTime UpdatedDate { get; set; }
-}
