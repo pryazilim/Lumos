dfbe3384e8df68e125cda40d6ff6c16e0a71063e
Emirhan Vurhan <emirhan@pryazilim.com>
2023-01-16 16:08:26 +0300

    Connection string bugfix

--- Database/DataContext.cs
+++ Database/DataContext.cs
@@ -2,11 +2,13 @@ using Microsoft.EntityFrameworkCore;
 
 public class DataContext : DbContext
 {
+  public static string ConnectionString = "Data Source=data.pryazilim.com,10000;Initial Catalog=admin_lumos;User ID=lumoshasar;Password=2eo0!U7a2;TrustServerCertificate=True;";
+
   public DbSet<AdminUser>? AdminUser { get; set; }
 
   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
   {
-    optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("Lumos_Database_ConnectionString"));
+    optionsBuilder.UseSqlServer(ConnectionString);
 
     base.OnConfiguring(optionsBuilder);
   }
