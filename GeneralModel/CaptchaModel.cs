9c07420675aa301d74f61b150b1822fc0ee99fc2
Cenk Erbakan <cenk@pryazilim.com>
2023-01-17 14:19:29 +0300

    captcha model added

---
+++ GeneralModel/CaptchaModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lumos.GeneralModel
+{
+    public class CaptchaModel
+    {
+         public bool success { get; set; }
+    }
+}
