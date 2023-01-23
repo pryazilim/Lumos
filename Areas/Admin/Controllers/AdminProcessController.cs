89a55d67f9638c74a324ff27a6aba54ed779d86c
Cenk Erbakan <cenk@pryazilim.com>
2023-01-23 18:30:40 +0300

    process page created

---
+++ Areas/Admin/Controllers/AdminProcessController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lumos.Areas.Admin.Controllers
+{
+    public class AdminProcessControllera
+    {
+
+      
+
+    }
+}
