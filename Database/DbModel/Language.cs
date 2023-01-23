8d40feff24ff380222164683b6b9aa9088f0d7a8
Cenk Erbakan <cenk@pryazilim.com>
2023-01-23 18:32:13 +0300

    language model created

---
+++ Database/DbModel/Language.cs
@@ -0,0 +1,10 @@
+
+  public class LanguageModel
+  {
+    public int? LanguageID { get; set; }
+    public string? LanguageName { get; set; }
+    public string? DisplayName { get; set; }
+    public string? VariableCode { get; set; }
+    public string? Value { get; set; }
+  }
+
